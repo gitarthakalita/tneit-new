064c5af27750fd1be19c963aa146880a6e5e1c15
KASHYAPYAN <indiawebdesign.seo@gmail.com>
2021-07-06 23:04:09 +0530

    wip

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -8,9 +8,9 @@ const Layout = ({ children, showAds }) => {
   useEffect(() => {
     let addHeight;
     if (showAds === "true") {
-      addHeight = 180 + 309.19;
+      addHeight = 160 + 309.19;
     } else {
-      addHeight = 180;
+      addHeight = 160;
     }
 
     window.onscroll = scrollShow;
