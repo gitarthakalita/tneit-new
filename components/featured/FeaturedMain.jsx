2e98c192936b7b0a2030dd945266044e0dac048b
KASHYAPYAN <indiawebdesign.seo@gmail.com>
2021-07-15 21:34:36 +0530

    wip

---
+++ components/featured/FeaturedMain.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FeaturedMain = () => {
+    return (
+        <div className="featured-main-wrapper">
+            
+        </div>
+    )
+}
+
+export default FeaturedMain
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,10 +5,22 @@ import Layout from '../components/layout/Layout'
 
 const Homepage = () => {
   return (
-    <Layout showAds= "true">
+    <Layout 
+    // showAds= "false"
+    >
       <section className="featured-section">
-        
+               <div className="wrapper">
+                 <div className="featured-live-updates"></div>
+                 <div className="featured-main"></div>
+                 <div className="featured-exclussive">
+                   <div className="exclussive-item"></div>
+                   <div className="exclussive-item"></div>
+                   <div className="exclussive-item"></div>
+                 </div>
+                 <div className="featured-opinion"></div>
+               </div>
       </section>
+      
      
     </Layout>
   )
