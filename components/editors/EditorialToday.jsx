81f4485a37e3b4d11e38e75fbc3513903502cb0d
KASHYAPYAN <indiawebdesign.seo@gmail.com>
2021-09-01 12:55:32 +0530

    wip

--- components/editors/EditorialToday.jsx
+++ components/editors/EditorialToday.jsx
@@ -1,9 +1,15 @@
+import { Button } from '@material-ui/core'
 import React from 'react'
 
 const EditorialToday = () => {
     return (
         <div className="editorial-today-card">
-            
+            <Button className="editorial-label">Editorial</Button>
+           <div className="editorial-wrapper">
+           <h3>China dam over Brahmaputra will be a disaster</h3>
+            <p>China is approving a mega dam on the world’s largest and the deepest gorge over the Tsangpo/ Siang/ Brahmaputra. Actually, it is a Giga dam as it is going to produce 60GW of energy. China’s aim to achieve carbon neutrality by 2060 by building a Giga dam for cheap electricity is outrageous. China already has an excess of energy, who will benefit from this electricity done at the expense of billions of people, including the death of the river Siang/Brahmaputra? </p>
+            <div className="editor-name">Editor-In-Chief, The Northeast India Times</div>
+           </div>
         </div>
     )
 }
