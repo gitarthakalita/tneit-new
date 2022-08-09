6d4cb0b130b7be3799484713b9ad8d473d998300
gitarthakalita <gitarthakalita.prsnl@gmail.com>
2022-08-09 15:16:34 +0530

    wip

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,20 @@
+
+
+import React from 'react';
+
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
 import '../styles/styles.scss'
 import '../styles/main.scss'
+
+
+
+
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
