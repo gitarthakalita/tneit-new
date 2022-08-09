d79c538de383c981893ae94aeec849469b096afa
gitarthakalita <gitarthakalita.prsnl@gmail.com>
2022-08-09 15:16:18 +0530

    wip

---
+++ components/navbar/MobileNavbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import MenuIcon from '@material-ui/icons/Menu';
+import ViewModuleIcon from '@material-ui/icons/ViewModule';
+import HomeIcon from '@material-ui/icons/Home';
+import Link from 'next/link';
+
+
+
+const MobileNavbar = () => {
+  return (
+    <div className='mobile-navbar-container'>
+      <div className="wrapper">
+
+        <Link href="/">
+          <div className='menu-item'>
+            <HomeIcon />
+            <span>Today</span>
+          </div>
+        </Link>
+
+
+        <Link href="/">
+          <div className='menu-item'>
+            <ViewModuleIcon />
+            <span>Sections</span>
+          </div>
+
+        </Link>
+
+        <Link href="/">
+          <div className='menu-item'>
+            <MenuIcon />
+            <span>Menu</span>
+          </div>
+        </Link>
+
+      </div>
+    </div>
+  )
+}
+
+export default MobileNavbar
+
+
